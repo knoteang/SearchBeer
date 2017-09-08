89e0cdc41bb34f1ef41d366cf553e1d3ce23bf22
Swiss <swiss.knot@g.swu.ac.th>
2017-09-08 22:08:06 +0700

    search beer v0.0.1

---
+++ js/index.js
@@ -0,0 +1,41 @@
+const getPunkApi = (param) => fetch('https://api.punkapi.com/v2/beers'+param)
+    .then(response => response.json())
+    .catch(e => console.error('Feil'))
+    .then(response => {
+        var result = response;
+        document.getElementById('output').innerHTML = "";
+        for (let i = 0; i < result.length; i++) {
+          document.getElementById('output').innerHTML = document.getElementById('output').innerHTML +
+          '<div style="word-wrap: break-word;padding: 10px;background-color:white;margin:4% 4% 4% 4%;opacity:0.8;text-align:canter;" class="col-md-3">'+
+            '<p style="font-size:30px;font-weight: bold;">'+result[i].name+'</p><img src="'+result[i].image_url+'" width="70%">'+
+            '<p style="font-size:20px;font-weight: bold;text-align:left;padding-top:50px;">Description : '+result[i].description+'</p>'+
+            '<p style="font-size:20px;font-weight: bold;text-align:left;">First brewed : '+result[i].first_brewed+'</p>'+
+            '<p style="font-size:20px;font-weight: bold;text-align:left;">Tagline : '+result[i].tagline+'</p>'+
+            '<p style="font-size:20px;font-weight: bold;text-align:left;">Yeast : '+result[i].ingredients.yeast+'</p>'+
+            '<p style="font-size:20px;font-weight: bold;text-align:left;">Food pairing : '+result[i].food_pairing+'</p>'+
+            '</div>';
+        }
+      });
+
+const sendParam = (Bname,Yname,Hname,Fname) => {
+  if (Bname == "" && Yname == "" && Hname == "" && Fname == "" ) {
+    getPunkApi(" ");
+  }else{
+    var param = "?";
+    if (Bname != "") {
+      param = param + "beer_name=" + Bname;
+    }
+    if (Yname != "") {
+      param = param + "&yeast=" + Yname;
+    }
+    if (Hname != "") {
+      param = param + "&hops=" + Hname;
+    }
+    if (Fname != "") {
+      param = param + "&food=" + Fname;
+    }
+    getPunkApi(param);
+  }
+}
+
+getPunkApi(" ");
